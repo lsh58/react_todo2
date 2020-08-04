4f7b92b1db3ba1e2488a7a0874b40967f10135dc
lsh58 <tmdgusdl58@gmail.com>
2020-08-04 17:54:41 +0900

    Updates

---
+++ precache-manifest.98b1f28812a5b7158eb25070e01a6116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfbc06ef60babd0b268dcb6b50ba443",
+    "url": "/react_todo2/index.html"
+  },
+  {
+    "revision": "76a2a2b0cb7e5c071b4e",
+    "url": "/react_todo2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4470f2567faf824519bf",
+    "url": "/react_todo2/static/js/2.2064f1f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_todo2/static/js/2.2064f1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a2a2b0cb7e5c071b4e",
+    "url": "/react_todo2/static/js/main.fce28276.chunk.js"
+  },
+  {
+    "revision": "536103bf55ebc35c8c33",
+    "url": "/react_todo2/static/js/runtime-main.a2e4e5b2.js"
+  }
+]);
